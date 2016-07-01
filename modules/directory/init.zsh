38a19617a93ed957ed15ef298a7f21fe74d41ad5
Eric Noyau <noyau@chromium.org>
2016-07-01 13:54:21 +0200

    Cutomize

--- modules/directory/init.zsh
+++ modules/directory/init.zsh
@@ -12,6 +12,7 @@
 
 setopt AUTO_CD              # Auto changes to a directory without typing cd.
 setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_MINUS          # cd -1 go to the previous
 setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
 setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
 setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
